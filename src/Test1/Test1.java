2d25a9b721ba40554041e8b87ab4756c4e0b2a85
arti <arthur.poberezhets@gmail.com>
2017-01-15 17:19:21 +0200

    Initial commit

---
+++ src/Test1/Test1.java
@@ -0,0 +1,11 @@
+package Test1;
+
+/**
+ * Created by admin on 15.01.2017.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        final int CONST1 = 123;
+        System.out.println(CONST1);
+    }
+}
